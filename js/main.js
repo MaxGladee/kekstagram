f246bcdfd1f20bf5da1b78dda6b50599e05c5dcd
MaxGlade <maks_v123@mail.ru>
2025-12-20 22:01:17 +0500

    Реализована форма отправки фотографии, добавлена библиотека Pristine и модуль form.js добавлен в index.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { createPhotos } from './photos.js';
 import { renderPictures } from './render.js';
 import { initBigPicture, attachPhotoClickHandlers } from './full-picture.js';
+import { initForm } from './form.js';
 
 const PHOTOS = createPhotos({ count: 25 });
 export { PHOTOS as photos };
@@ -9,4 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
   renderPictures();
   initBigPicture();
   attachPhotoClickHandlers();
+  initForm();
 });
